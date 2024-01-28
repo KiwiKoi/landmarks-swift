294d2c14cc645d9c3ee388c48bfc78bda96845b7
Daniel <danielo.visage@gmail.com>
2024-01-28 14:10:54 +0100

    SwiftUI essentials: Handling user input

---
+++ Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Daniel Visage on 28/01/2024.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button{
+            isSet.toggle()
+        } label: {
+            Label("Toggle favorite", systemImage: isSet ? "star.fill":"star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
