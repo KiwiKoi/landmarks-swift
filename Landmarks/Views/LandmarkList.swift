86c6cbf5f78ea28429536ada7d5fbb74f0f2aa0e
Daniel <danielo.visage@gmail.com>
2024-01-28 13:48:20 +0100

    SwiftUI essentials: Building lists and navigation

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Daniel Visage on 28/01/2024.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Daniel Visage on 28/01/2024.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
